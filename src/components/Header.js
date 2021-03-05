16b7a2f26c9c3107d8b9cbc7c6028fb935cfe00f
MohamedSaafan <201827254@std.sci.cu.edu.eg>
2021-03-05 16:58:30 +0200

    removed the login and the market data bar and added the instant swap features

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,41 +3,48 @@ import React from "react";
 import Styles from "./Header.module.css";
 import MainNavBar from "./MainNavBar";
 import { Link } from "react-router-dom";
-import {connect} from 'react-redux';
+import { connect } from "react-redux";
 
 const Header = (props) => {
- 
-  
+  const handleSwap = () => {
+    window.zeroExInstant.render(
+      {
+        orderSource: "https://api.0x.org/sra/",
+        affiliateInfo: {
+          feeRecipient: "0xbb99ec9496007cfc2d965ce65543c4f8398163be",
+          feePercentage: 0.02,
+        },
+      },
+      "body"
+    );
+  };
   return (
     <header className={Styles.mainHeader}>
       <MainNavBar
         styles={Styles}
         shouldManipulate={true}
-        isAdmin = {props.isAdmin}
+        isAdmin={props.isAdmin}
       />
 
-      <div className="container">
+      <div className='container'>
         <div className={Styles.headerContent}>
-          <h1>Cardano StakePools</h1>
+          <h1>AltsSwap</h1>
 
           <p>
-            {" "}
-            Learn with Us and now Earn with us. Our Cardano stakepools are now
-            live.
+            the most convenient way to swap your crypto, while supporting your
+            group too!
           </p>
-          <Link to="/stakepool">
-            <button>learn more</button>
-          </Link>
+
+          <button onClick={handleSwap}>Instant Swap</button>
         </div>
       </div>
     </header>
   );
 };
-const mapStateToProps = state => {
-const {isAdmin} = state.auth
-return {
-  isAdmin
-}
- 
-}
+const mapStateToProps = (state) => {
+  const { isAdmin } = state.auth;
+  return {
+    isAdmin,
+  };
+};
 export default connect(mapStateToProps)(Header);
